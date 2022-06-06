93ef53abb82e4d5803c3a40daf88ce6c48537333
LAPTOP-7DKKV3RP\dully <eunha732@hanmail.net>
2022-06-06 17:51:10 +0900

    스레드 생성 예제 추가

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -2,7 +2,33 @@
 #include <iostream>
 #include "CorePch.h"
 
+#include <thread>
+
+void HelloThread()
+{
+    cout << "Hello Thread" << endl;
+}
+
 int main()
 {
-    HelloWorld();
+    // 스레드의 생성
+    std::thread t(HelloThread);
+
+    cout << t.joinable() << endl;
+    
+    cout << "Hello Main" << endl;
+
+    cout << t.hardware_concurrency() << endl; // 동시 스레드 수
+
+    cout << t.joinable() << endl;
+
+    cout << t.get_id() << endl; // 스레드 아이디
+
+    if (t.joinable())
+    {
+        t.join();
+    }
+
+    //t.detach();
+  
 }
