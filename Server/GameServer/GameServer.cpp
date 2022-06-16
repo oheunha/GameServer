e46c92b0b7966e9663d6f759aee445e2a4f94233
LAPTOP-7DKKV3RP\dully <eunha732@hanmail.net>
2022-06-17 00:00:22 +0900

    [ lock basic ] mutex, lock, lock_guard, unique_lock 예제 - 스레드에서 컨테이너에 데이터를 삽입하는 연산을 할 때, 상호배타적으로 처리하지 않는 경우 부정확한 숫자가 저장됨 - 본 예제에서는 뮤텍스의 사용과  lock, lock_guard를 템플릿 클래스 형태로 이해해보고,  unique_lock에 대해서 알아봅니다.

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -1,8 +1,69 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
+#include <thread>
+#include <atomic>
+#include <mutex>
+
+
+//[1][2][3]
+//[1][2][3][][][] 
+
+vector<int32> v;
+
+// Mutual Exclusive (상호배타적)
+mutex m;
+
+
+// RAII (Resource Acquisition Is Initialization)
+template<typename T>
+class LockGuard
+{
+public:
+    LockGuard(T& m)
+    {
+        _mutex = &m;
+        _mutex->lock();
+    }
+
+    ~LockGuard()
+    {
+        _mutex->unlock();
+    }
+
+private:
+    T* _mutex;
+};
+
+void Push()
+{
+    for (int32 i = 0; i < 10000; i++)
+    {
+        // 자물쇠 잠그기
+        // LockGuard<std::mutex> lockGuard(m);
+        //std::lock_guard<std::mutex> lockGuard(m);
+        std::unique_lock<std::mutex> uniqueLock(m, std::defer_lock); // 당장 락을 잠그지 않음.
+        
+
+        uniqueLock.lock(); // 잠그는 시점을 뒤로 함
+
+        //m.lock();
+        v.push_back(i);
+
+        // 자물쇠 풀기
+        //m.unlock();
+    }
+}
 
 int main()
 {
-    HelloWorld();
+    v.reserve(20000);
+    std::thread t1(Push);
+    std::thread t2(Push);
+
+    t1.join();
+    t2.join();
+
+    cout << v.size();
+
 }
