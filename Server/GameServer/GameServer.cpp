fa0f9374c535d3f32324a4ae50d21869bdca00bb
oheunha <eunha732@hanmail.net>
2022-06-17 21:18:09 +0900

    [ 스핀락 ] 스핀락 클래스를 생성하고 lock 함수를 정의하였으나 여러 쓰레드가 동시에 while문 안으로 들어간 후, _.locked 멤버를  true로 세팅하여 올바른 결과(기대값:0)가 나오지 않는 상황입니다.

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -5,7 +5,62 @@
 #include <atomic>
 #include <mutex>
 
+class SpinLock
+{
+public:
+    void lock()
+    { 
+        while (_locked)
+        {
+
+        }
+
+        _locked = true;
+    }
+
+    void unlock()
+    {
+        _locked = false;
+
+    }
+
+private:
+    volatile bool _locked = false;
+
+
+};
+
+SpinLock spinLock;
+int32 sum = 0;
+mutex m;
+
+void Add()
+{
+    for (int32 i = 0; i < 10'0000; i++)
+    {
+        lock_guard<SpinLock>guard(spinLock);
+        sum++;
+    }
+}
+
+void Sub()
+{
+    for (int32 i = 0; i < 10'0000; i++)
+    {
+        lock_guard<SpinLock>guard(spinLock);
+        sum--;
+    }
+}
+
 int main()
 {
-    
+    thread t1(Add);
+    thread t2(Sub);
+
+    t1.join();
+    t2.join();
+
+
+    cout << sum << endl;
+
 }
