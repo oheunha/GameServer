f56a767ed98743a65a812b26db8cc15d9c5e303f
oheunha <eunha732@hanmail.net>
2022-06-18 00:32:40 +0900

    [ Event ] 이벤트를 이용한 동기화 기법 해당 예제의 경우 자동이벤트(false)로 설정하였으며, 1) Producer함수에서 데이터를 삽입 후, 이벤트를 Signaled로 설정합니다. 2) Consumer함수에서 WaitForSingleObject로 핸들의 신호를 확인하고 켜져있다면 무한루프 안의 내용을 실행합니다.

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -4,8 +4,61 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <windows.h>
 
-int main()
+mutex m;
+queue<int32> q; // 공용 데이터 관리
+HANDLE handle;
+void Producer() // 데이터 생성
 {
+    while (true)
+    {
+        {
+            unique_lock<mutex> lock(m);
+            q.push(100);
+        }
+
+        ::SetEvent(handle); // Signaled 상태로 세팅
+
+        this_thread::sleep_for(10000ms);
+    }
+}
+
+void Consumer() // 데이터를 소모
+{
+    while (true)
+    {
+        ::WaitForSingleObject(handle, INFINITE); // 이벤트가 Non-Signaled면 이 스레드는 잠들게 되며 ( 대기쿨쿨 ), Signaled면 아래 코드를 동작합니다.
+        // ::ResetEvent(handle); 수동이면 직접 신호를 꺼주어야한다.
+
+        unique_lock<mutex> lock(m);
+        if (q.empty() == false)
+        {
+            int32 data = q.front();
+            q.pop();
+            cout << data << endl;
+        }
+    }
+}
+
+
+
+int main( )
+{
+    // 데이터가 있을 때만 알려주세요.
+    // 커널 오브젝트
+    // Usage Count
+    // Signal (파란불) / Non-Signal (빨간불) << bool
+    // Auto / Manual << bool
+    handle = ::CreateEvent(NULL/*보안속성*/, FALSE/*bManualReset*/, FALSE/*bInitialState*/, NULL);
+
+
+
+    thread t1(Producer);
+    thread t2(Consumer);
     
+    t1.join();
+    t2.join();
+
+    ::CloseHandle(handle);
 }
