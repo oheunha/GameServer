5d5d43e4092e8730575ac76067c91eb2aed1f057
oheunha <eunha732@hanmail.net>
2022-06-18 15:45:22 +0900

    [ 캐시 ] 인접한 데이터 접근 연산후 시간값과 멀리 떨어진 데이터 접근 연산후 시간값을 통하여 캐시의 철학을 확인할 수 있다. < 캐시 철학 > 1) TEMPORAL LOCALITY 시간적으로 볼 때, 최근 접근한 데이터를 또 접근할 확률이 높음 2) SPATIAL LOCALITY 방금 주문 옆에서 주문확률 높음을 빗댈 수 있는데, 이를 테면, for문 실행 시 순차적 접근, 즉 인접 접근할 확률이 높다.
    위 2가지 철학을 토대로 캐시에는 최근 접근했던 데이터의 주변데이터를 적재해 놓도록 설계되어있으며
    예제에서 시간차이를 통하여 확인할 수 있다.

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -4,8 +4,49 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <Windows.h>
+#include <future>
+
+// [][][][][][]     [][][][][][]        [][][][][][]        [][][][][][]
+
+int64 buffer[10000][10000];
 
 int main()
 {
-    
+    memset(buffer, 0, sizeof(buffer));
+
+    {
+        uint64 start = GetTickCount64();
+
+        int64 sum = 0;
+        for (int32 i = 0; i < 10000; i++)
+        {
+            for (int32 j = 0; j < 10000; j++)
+            {
+                sum += buffer[i][j];
+
+            }
+        }
+
+        uint64 end = GetTickCount64();
+        cout << "Elapsed Tick " << (end - start) << endl;
+    }
+
+
+    {
+        uint64 start = GetTickCount64();
+
+        int64 sum = 0;
+        for (int32 i = 0; i < 10000; i++)
+        {
+            for (int32 j = 0; j < 10000; j++)
+            {
+                sum += buffer[j][i];
+
+               
+            }
+        }
+        uint64 end = GetTickCount64();
+        cout << "Elapsed Tick " << (end - start) << endl;
+    }
 }
