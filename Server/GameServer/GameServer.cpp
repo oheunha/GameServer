28ac8e404ca6cc1647300ce9063767f7919e93f4
oheunha <eunha732@hanmail.net>
2022-06-18 15:15:27 +0900

    [ Future ] 미래에 결과물을 반환받으며 옵션에 따라 별도의 쓰레드가 만들어질 수도, 아닐 수 도 있다. deferred 옵션 의 경우 지연실행, async 옵션은 별도의 스레드를 만들어서 실행합니다. - promise와 짝을 맞추어 사용하기도 합니다. - packaged_task는 일감이라는 단위로 다른쪽 스레드에서 호출하도록 합니다.

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -4,8 +4,98 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <future>
+
+int64 Calculate()
+{
+    int64 sum = 0;
+    
+    for (int32 i = 0; i < 100'000; i++)
+        sum += i;
+
+    return sum;
+
+}
+
+void PromiseWorker(std::promise<string>&& promise)
+{
+    promise.set_value("Secret Message");
+}
+
+void TaskWorker(std::packaged_task<int64(void)>&& task)
+{
+    task();
+}
 
 int main()
 {
-    
+    // 동기(synchronous) 실행
+    //int64 sum = Calculate();
+    //cout << sum << endl;
+
+    // std::future
+    {
+        // 1) deferred -> lazy evalation 지연해서 실행하세요
+        // 2) async -> 별도의 쓰레드를 만들어서 실행하세요
+        // 3) deferred | async -> 둘 중 알아서 골라주세요
+        
+        // 언젠가 미래에 결과물을 뱉어줄거야!
+        std::future<int64> future = std::async(std::launch::async, Calculate);
+
+       // TODO
+        
+       /*
+            // 1ms를 줄테니 그동안 일을 끝냈는지 확인
+            std::future_status status = future.wait_for(1ms);
+            if (status == future_status::ready) // 일감이 완료된 상태라면
+            {
+                // ...
+            }
+
+       
+            // future.wait(); // wait_for를 무한정 기다리는 기능, 생략가능.
+        */
+
+        int64 sum = future.get(); // 결과물이 이제야 필요하다!
+
+        class Knight
+        {
+        public:
+            int64 GetHp() { return 100; }
+        };
+
+        Knight knight;
+        std::future<int64> future2 = std::async(std::launch::async, &Knight::GetHp, knight); // knight.GetHp();
+
+
+    }
+
+
+    // std::promise
+    {
+        // 미래(std::future)에 결과물을 반환해줄꺼라 약속(std::promise) 해줘~ (계약서?)
+        std::promise<string> promise;
+        std::future<string> future = promise.get_future();
+
+        thread t(PromiseWorker, std::move(promise));
+
+        string message = future.get();
+        cout << message << endl;
+
+        t.join();
+    }
+
+    // std::packaged_task
+    {
+        std::packaged_task<int64(void)> task(Calculate);
+        std::future<int64> future = task.get_future();
+
+        std::thread t(TaskWorker, std::move(task));
+
+        int64 sum = future.get();
+        cout << sum << endl;
+
+        t.join();
+    }
+
 }
