0d0a24a911708aba19b75a421e3b528a05fb708d
oheunha <eunha732@hanmail.net>
2022-07-10 23:27:13 +0900

    compare_exchange_weak과 compare_exchage_stong - intel 머신에서 둘의 성능은 별 차이 없음. - compare_exchange_weak은 묘한 상황에서 실패하는 경우가 있음( 가짜실패 ), while코드와 사용 - compare_exchange_strong은 weak의 묘한 상황 실패를 보완하나 내부적으로 루프를 한번 더 돌기에 부하가 더 있다.

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -6,8 +6,56 @@
 #include <mutex>
 #include <Windows.h>
 #include <future>
+atomic<bool> flag;
 
 int main()
 {
+    flag = false;
+
+    //flag = true;
+    flag.store(true, memory_order::memory_order_seq_cst); // default
+
+    //bool val = flag;
+    bool val = flag.load(memory_order::memory_order_seq_cst); // default
+
+    // 이전 flag 값을 prev에 넣고, flag 값을 수정
+    {
+        bool prev = flag.exchange(true);
+        //bool prev = flag;
+        //flag = true;
+    }
+
+    // CAS (Compare-And-Swap) 조건부 수정
+    {
+        bool expected = false;
+        bool desired = true;
+        flag.compare_exchange_strong(expected, desired);
+
+        // Spurious Failure
+        if (flag == expected)
+        {
+            // 다른 쓰레드의 interreption을 받아서 중간에 실패할 수 있음
+            // if (묘한 상황)
+            //  return false;
+
+
+            //expected = flag;
+            flag = desired;
+            return true;
+        }
+        else
+        {
+            expected = flag;
+            return false;
+        }
+
+        while (true)
+        {
+            bool expected = false;
+            bool desired = true;
+            flag.compare_exchange_weak(expected, desired);
+        }
+
+    }
     
 }
