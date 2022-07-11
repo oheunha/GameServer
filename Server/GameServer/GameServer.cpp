d2971db28c4b2e7585cd786e42ee479b38b82158
oheunha <eunha732@hanmail.net>
2022-07-12 01:35:16 +0900

    Thread Local Storage란 스레드 마다 가지고 있는 로컬 저장소 입니다. thread_local 키워드로 TLS에서의 변수선언을 할 수 있습니다.

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -7,7 +7,31 @@
 #include <Windows.h>
 #include <future>
 
+//_declspec(thread) int32 value; 예전의 TLS사용 
+thread_local int32 LThreadId = 0; // 일반 변수가 아닌 TLS 영역으로 잡힘
+thread_local queue<int32> q;
+
+void ThreadMain(int32 threadId)
+{
+	LThreadId = threadId;
+	while (true)
+	{
+		cout << "Hi! I am Thread " << LThreadId << endl;
+		this_thread::sleep_for(1s);
+	}
+}
+
 int main()
 {
-    
+	vector<thread> threads;
+
+	for (int32 i = 0; i < 10; ++i)
+	{
+		int32 threadId = i + 1;
+		threads.push_back(thread(ThreadMain, threadId));
+	}
+
+	for (thread& t : threads)
+		t.join();
+  
 }
