429a85d75dd924f379a848a44d3ce59927194f2a
oheunha <eunha732@hanmail.net>
2022-07-16 14:40:52 +0900

    컴파일 에러 수정

--- Server/GameServer/ConcurrentStack.h
+++ Server/GameServer/ConcurrentStack.h
@@ -14,7 +14,7 @@ public:
 	void Push(T value)
 	{
 		lock_guard<mutex> lock(_mutex);
-		_stack.push(std::move());
+		_stack.push(std::move(value));
 		_condVar.notify_one(); // 대기하고 있는 애 하나 꺼내기, 시그널
 	}
 
@@ -47,3 +47,99 @@ private:
 };
 	 
 
+template<typename T>
+class LockFreeStack
+{
+	struct Node
+	{
+		Node(const T& value) : data(value)
+		{
+
+		}
+
+		T data;
+		Node* next;
+	};
+
+public:
+	// 1) 새 노드를 만들고
+	// 2) 새 노드의 next = head
+	// 3) head = 새 노드
+	// [value][][][][][]
+	// [head]
+	void Push(const T& value)
+	{
+		Node* node = new Node(value);
+		node->next = _head;
+		
+		/*
+			if (_head == node->next)
+			{
+				_head = node;
+				return true;
+			}
+			else
+			{
+				node->next = _head;
+				return false;
+			}
+		*/
+		while (_head.compare_exchange_weak(node->next, _head) == false)
+		{
+			// node->next = _head;
+		}
+
+		// 이 사이에 새치기 당하면?
+		//_head = node;
+
+	}
+
+	// 1) head 읽기
+	// 2) head->next 읽기
+	// 3) head = head->next
+	// 4) data 추출해서 반환
+	// 5) 추출한 노드를 삭제
+	
+	// [][][][][][]
+	// [head]
+	bool TryPop(T& value)
+	{
+		Node* oldHead = _head;
+		
+		/*
+		if (_head == oldHead)
+		{
+			_head = oldHead->next;
+			return true;
+		}
+		else
+		{
+			oldHead = _head;
+			return false;
+		}
+		*/
+		while (oldHead && _head.compare_exchange_weak(oldHead, oldHead->next) == false)
+		{
+			//oldHead = _head;
+		}
+		
+		if (oldHead == nullptr)
+		{
+			return false;
+		}
+
+		// Exception X
+		value = oldHead->data;
+		
+		// 잠시 삭제 보류
+		//delete oldHead;
+
+		
+		return true;
+	}
+
+private:
+	// [][][][][][]
+	// [head]
+	atomic<Node*> _head;
+};
