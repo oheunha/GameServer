31a44de263caf0e6cf252228d4f28a012528fa3f
oheunha <eunha732@hanmail.net>
2023-02-05 00:21:33 +0900

    여러개의 스레드 예제 추가

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -9,10 +9,29 @@ void HelloThread()
     cout << "Hello Thread" << endl;
 }
 
+void HelloThread_2(int32 num)
+{
+    cout << "[2]" << num << endl;
+}
+
 int main()
 {
     // 스레드의 생성
     std::thread t(HelloThread);
+
+    vector<std::thread> v;
+    for (int32 i = 0; i < 10; ++i)
+    {
+        v.push_back(std::thread(HelloThread_2, i));
+    }
+
+    for (int32 i = 0; i < 10; ++i)
+    {
+        if (v[i].joinable())
+        {
+            v[i].join();
+        }
+    }
 
     cout << t.joinable() << endl;
     
