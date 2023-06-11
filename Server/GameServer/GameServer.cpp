43c086b34a5dd6cd66bb13f2acddf37ed6388d3e
oheunha <eunha732@hanmail.net>
2023-06-12 07:53:20 +0900

    [ Atomic ] 공유 데이터 문제점을 해결해주는 atomic
    - All or Nothing 둘중 하나로 동작
    - cpu가 완료될 때까지 순서를 실행하고 막아주며 instruction을 보장해줌
    - 멀티스레드 환경 atomic 연산 막 사용하면 안됨
    - 하나 실행 시 다른 것이 실행되지 않으므로 병목현상이 일어날 수 있음

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -7,7 +7,53 @@
 #include <Windows.h>
 #include <future>
 
+// atomic : All-Or-Nothing
+atomic<int32> sum = 0;
+
+// DB
+//
+// A라는 유저 인벤에서 집행검을빼고
+// B라는 유저 인벤에 집행검 추가
+
+
+void Add()
+{
+    for (int32 i = 0; i < 100'0000; i++)
+    {
+        sum.fetch_add(1);
+        //sum++;
+        /*
+            int32 =  eax = sum;
+            eax = eax + 1;
+            sum = eax;
+        */
+    }
+}
+
+void Sub()
+{
+    for (int32 i = 0; i < 100'0000; i++)
+    {
+        sum.fetch_sub(1);
+        //sum--;
+        /*
+          int32 =  eax = sum;
+          eax = eax - 1;
+          sum = eax;
+      */
+    }
+}
+
 int main()
 {
-    
+    //Add();
+    //Sub();
+    //
+    std::thread t1(Add);
+    std::thread t2(Sub);
+
+    t1.join();
+    t2.join();
+    cout << sum << endl;
+
 }
