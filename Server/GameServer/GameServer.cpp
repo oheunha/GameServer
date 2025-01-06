3275a764c0c86a34e2887199f31fe8a63d14e8f2
oheunha <eunha732@hanmail.net>
2025-01-07 02:11:25 +0900

    27_연습문제(소수 합구하기)
    -멀티스레드 일감 던저서 각 스레드들이 작업해서 소수의 합을 구한다.

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -8,31 +8,70 @@
 #include <future>
 #include "CoreMacro.h"
 #include "ThreadManager.h"
-#include "AccountManager.h"
-#include "PlayerManager.h"
 
+#include <vector>
+#include <thread>
+
+// 소수 구하기
+bool isPrime(int number)
+{
+	if (number <= 1)
+		return false;
+	if (number == 2 || number == 3)
+		return true;
+
+	for (int i = 2; i < number; i++)
+	{
+		if ((number % i) == 0)
+			return false;
+	}
+	return true;
+}
+
+int CoountPrime(int start, int end)
+{
+	int count = 0;
+
+	for (int number = start; number <= end; number++)
+	{
+		if (isPrime(number))
+			count++;
+	}
+	return count;
+}
+
+// 1과 자기 자신으로만 나뉘면 그것을 소수라고 함
+// 2,3,5,7,11
 
 int main()
 {
-	GThreadManager->Launch([=]
-	{
-		while(true)
-		{
-			cout << "PlayerThenAccount" << endl;
-			GPlayerManager.PlayerThenAccount();
-			this_thread::sleep_for(100ms);
-		}
-	});
+	const int MAX_NUMBER = 1000'0;
 
-	GThreadManager->Launch([=]
-		{
-			while (true)
+	// 1000 = 168
+	// 10'000 = 1229
+	// 1'000'000 = 78498
+
+	// 1~MAX_NUMBER까지의 소수 개수
+	vector<thread> threads;
+
+	int coreCount = thread::hardware_concurrency(); // 코어갯수 실시간 병렬실행할 수 있는 개수
+	int jobCount = (MAX_NUMBER / coreCount) + 1;
+
+	atomic<int> primeCount = 0; // 결과물
+
+	for (int i = 0; i < coreCount; i++)
+	{
+		int start = (i * jobCount) + 1;
+		int end = min(MAX_NUMBER, ((i + 1) * jobCount));
+
+		threads.push_back(thread([start, end, &primeCount]()
 			{
-				cout << "AccountTheanPlayer" << endl;
-				GAccountManager.AccountThenPlayer();
-				this_thread::sleep_for(100ms);
-			}
-		});
-		
-	GThreadManager->Join();
+				primeCount += CoountPrime(start, end);
+			}));
+	}
+
+	for (thread& t : threads)
+		t.join();
+
+	cout << primeCount << endl;
 }
